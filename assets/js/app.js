a4f81c8f20e9a3d9438b9b282219348fe9cc71b4
Giustino Borzacchiello <giustinob@gmail.com>
2015-08-05 08:08:22 +0200

    First commit - Static View

---
+++ assets/js/app.js
@@ -0,0 +1,35 @@
+var ContactManager = new Marionette.Application();
+
+ContactManager.navigate = function (route, options) {
+  if (options === undefined) {
+    options = {};
+  }
+  Backbone.history.navigate(route, options);
+};
+
+ContactManager.getCurrentRoute = function () {
+  return Backbone.history.fragment;
+};
+
+ContactManager.on('before:start', function () {
+
+  var RegionContainer = Marionette.LayoutView.extend({
+    el: '#app-container',
+
+    regions: {
+      main: '#main-region'
+    }
+  });
+
+  ContactManager.regions = new RegionContainer();
+});
+
+ContactManager.on('start', function () {
+  if (Backbone.history) {
+    Backbone.history.start();
+
+    if (this.getCurrentRoute() === '') {
+      ContactManager.trigger('contacts:list');
+    }
+  }
+});
---
+++ assets/js/apps/contacts/list/list_controller.js
@@ -0,0 +1,26 @@
+ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbone, Marionette, $, _){
+  List.Controller = {
+    listContacts: function(){
+      var loadingView = new ContactManager.Common.Views.Loading();
+      ContactManager.regions.main.show(loadingView);
+
+      var fetchingContacts = ContactManager.request("contact:entities");
+
+      $.when(fetchingContacts).done(function(contacts){
+        var contactsListView = new List.Contacts({
+          collection: contacts
+        });
+
+        contactsListView.on("childview:contact:show", function(childView, model){
+          ContactManager.trigger("contact:show", model.get("id"));
+        });
+
+        contactsListView.on("childview:contact:delete", function(childView, model){
+          model.destroy();
+        });
+
+        ContactManager.regions.main.show(contactsListView);
+      });
+    }
+  }
+});
